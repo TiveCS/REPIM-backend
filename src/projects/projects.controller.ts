3d8e1457c0bb7101b5d4c4d4debc1385b531f035
TiveCS <ahmadfathanhanif@gmail.com>
2022-10-18 11:40:55 +0700

    implement basic endpoint

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { ProjectsService } from './projects.service';
+import { JwtAuthGuard } from './../auth/guard/jwt.guard';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { GetUser } from 'src/users/decorator/user.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller('projects')
-export class ProjectsController {}
+export class ProjectsController {
+  constructor(private readonly projectsService: ProjectsService) {}
+
+  @Post()
+  async createProject(
+    @Body() dto: CreateProjectDto,
+    @GetUser('id') ownerId: number,
+  ) {
+    return this.projectsService.createProject(dto, ownerId);
+  }
+
+  @Delete('/:id')
+  async deleteProjectById(
+    @Param('id', ParseIntPipe) projectId: number,
+    @GetUser('id') ownerId: number,
+  ) {
+    return this.projectsService.deleteProjectById(projectId, ownerId);
+  }
+
+  @Get()
+  async getProjectsByOwnerId(@GetUser('id') ownerId: number) {
+    return this.projectsService.getProjectsByOwnerId(ownerId);
+  }
+}
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
+import { PrismaService } from './../prisma/prisma.service';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(private prisma: PrismaService) {}
+
+  async createProject(dto: CreateProjectDto, ownerId: number) {
+    const project = await this.prisma.project.create({
+      data: {
+        ...dto,
+        owner: {
+          connect: {
+            id: ownerId,
+          },
+        },
+      },
+      select: {
+        id: true,
+        title: true,
+        createdAt: true,
+      },
+    });
+
+    return project;
+  }
+
+  async deleteProjectById(projectId: number, ownerId: number) {
+    await this.verifyProjectOwner(projectId, ownerId);
+
+    const project = await this.prisma.project.delete({
+      where: {
+        id: projectId,
+      },
+      select: {
+        id: true,
+        title: true,
+      },
+    });
+
+    return project;
+  }
+
+  async getProjectsByOwnerId(ownerId: number) {
+    const projects = await this.prisma.project.findMany({
+      where: {
+        ownerId,
+      },
+      select: {
+        id: true,
+        title: true,
+        createdAt: true,
+      },
+    });
+
+    return projects;
+  }
+
+  async verifyProjectOwner(projectId: number, ownerId: number) {
+    const project = await this.prisma.project.findUnique({
+      where: {
+        id: projectId,
+      },
+      select: {
+        ownerId: true,
+      },
+    });
+
+    if (!project) throw new NotFoundException('Project not found');
+
+    if (project.ownerId !== ownerId)
+      throw new ForbiddenException('You are not the owner of this project');
+  }
+}
